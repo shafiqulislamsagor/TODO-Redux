02701c53a3463c402fb6b8807fb84f9a8fa2ce3b
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2024-09-27 01:35:34 +0600

    todo container design

--- src/components/Todo/TodoCard.tsx
+++ src/components/Todo/TodoCard.tsx
@@ -1,15 +1,23 @@
+import Delete from "@/svg/Delete";
 import { Button } from "../ui/button";
+import { Checkbox } from "../ui/checkbox";
+import Edit from "@/svg/Edit";
 
 export default function TodoCard() {
   return (
-    <div className="bg-white rounded-md flex justify-between items-center p-3">
-      <input type="checkbox" name="" id="" />
+    <div className="bg-white rounded-md flex justify-between items-center p-3 border">
+      <Checkbox />
+
       <p className="font-semibold">Todo tile</p>
       <p>Time</p>
       <p>description</p>
       <div className="space-x-4">
-        <Button>delete </Button>
-        <Button>edit </Button>
+        <Button className="bg-red-500">
+          <Delete />
+        </Button>
+        <Button className="bg-purple-700">
+          <Edit />
+        </Button>
       </div>
     </div>
   );
--- src/components/Todo/TodoContainer.tsx
+++ src/components/Todo/TodoContainer.tsx
@@ -4,18 +4,20 @@ import TodoCard from "./TodoCard";
 export default function TodoContainer() {
   return (
     <div>
-      <div>
-        <Button>add todo</Button>
-        <Button>filter</Button>
+      <div className="flex justify-between items-center py-6">
+        <Button className="bg-primary-gradient">add todo</Button>
+        <Button className="bg-primary-gradient">filter</Button>
       </div>
-      <div className="bg-red-500 w-full  rounded-xl p-6 space-y-3">
-        {/* <div className="bg-white text-2xl font-bold p-5 flex justify-center items-center">
+      <div className="bg-primary-gradient w-full  rounded-xl p-1 ">
+        <div className="bg-white w-full h-full space-y-3 p-4 rounded-lg ">
+          {/* <div className="bg-white text-2xl font-bold p-5 flex justify-center items-center">
           <h1>There is no task pending</h1>
         </div> */}
-        <TodoCard />
-        <TodoCard />
-        <TodoCard />
-        <TodoCard />
+          <TodoCard />
+          <TodoCard />
+          <TodoCard />
+          <TodoCard />
+        </div>
       </div>
     </div>
   );
